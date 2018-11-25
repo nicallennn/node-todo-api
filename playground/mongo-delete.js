fc1a05642ae4820a90e74b314cd4c4c65092ec7b
Nick Allen <nick@scapplications.co.uk>
2018-11-25 15:20:32 +0000

    Tidied up delete methods

--- playground/mongo-delete.js
+++ playground/mongo-delete.js
@@ -22,32 +22,49 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
    * -> DELETE FROM USERS COLLECTION     *
    * ************************************/
 
-  // deleteMany -> target collection -> deleteMany where text property === 'Eat lunch' string -> callback
-  db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((res) => {
-    console.log(res)
-    //log no. of deleted documents
-    console.log(`${res.result.n} Tods were deleted!`)
-  })
+  const deleteMatchesText = (text, all) => {
+    //if all === true -> delete all documents, else -> delete first instance
+    if (all) {
+      // deleteMany -> target collection -> deleteMany where text property === 'Eat lunch' string -> callback
+      db.collection('Todos').deleteMany({ text: text }).then((res) => {
+        console.log(res)
+        //log no. of deleted documents
+        console.log(`${res.result.n} Todos were deleted!`)
+      })
+    } else {
+      // deleteOne -> traget collection -> delete first document where text property === 'Eat lunch' string -> callback
+      db.collection('Todos').deleteOne({ text: text }).then((res) => {
+        console.log(res)
+      })
+    }
+  }
 
-  // deleteOne -> traget collection -> delete first document where text property === 'Eat lunch' string -> callback
-  db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then((res) => {
-    console.log(res)
-  })
+  const deleteOneTodo = (completed) => {
+    if (completed) {
+      // findOneAndDelete -> deletes and returns deleted document object 
+      db.collection('Todos').findOneAndDelete({ completed: true }).then((res) => {
+        console.log(res)
+      })
+    } else {
+      db.collection('Todos').findOneAndDelete({ completed: false }).then((res) => {
+        console.log(res)
+      })
+    }
+  }
 
-  // findOneAndDelete -> deletes and returns deleted document object -> 
-  db.collection('Todos').findOneAndDelete({ completed: false }).then((res) => {
-    console.log(res)
-  })
+  const deleteAllUsersOnName = (name) => {
+    //deleteMany where username === 'Nick Allen'
+    db.collection('users').deleteMany({ name: name }).then((res) => {
+      console.log(`${res.result.n} records deleted!`)
+    })
+  }
 
-  //deleteMany where username === 'Nick Allen'
-  db.collection('users').deleteMany({ name: 'Nick Allen' }).then((res) => {
-    console.log(`${res.result.n} records deleted!`)
-  })
-
-  //findOneAndDelete where user _id === '5bfab9b42431972593ff8e2c'
-  db.collection('users').findOneAndDelete({ _id: new ObjectID('5bfab9b42431972593ff8e2c') }).then((res) => {
-    console.log(JSON.stringify(res, null, 2))
-  })
+  const deleteUserByID = (id) => {
+    //findOneAndDelete where user _id === id
+    db.collection('users').findOneAndDelete({ _id: new ObjectID(id) }).then((res) => {
+      console.log(JSON.stringify(res, null, 2))
+    })
+  }
 
   //close the connection
   client.close()
